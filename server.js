bef8fd7a25b3c4ba216e3c78a9fb6185c6896b25
kingbin <chris.blazek@gmail.com>
2014-11-22 17:52:59 -0600

    maybe?

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var app = express();
+var gzippo = require('gzippo')
+  , express = require('express')
+  , app = express()
+  , path = require('path');
 
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
+
+var winston = require('winston')
+  , logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: path.join(__dirname,'Logs','info.log') })
+    ]
+    , exceptionHandlers: [
+        new winston.transports.File({ filename: path.join(__dirname,'Logs','exceptions.log') })
+    ]
+    , exitOnError: false
+  });
+
+
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
